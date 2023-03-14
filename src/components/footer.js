549c1d7c5ac05407da0832d3148e0e46ed3dbb13
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-03-14 22:40:31 +0530

    online exam frontent completed

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer() {
+    return <footer>
+        <p>&copy; This project done by divyamala_</p>
+    </footer>;
+}
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header(){
+return <header>
+    <h1>Exam Portal</h1>
+</header>;
+}
